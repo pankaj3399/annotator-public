8fad48fb63d4beb2bb0c3d433c3057daf2cba678
rohin079 <rohinmehrotra@gmail.com>
2024-11-25 22:54:17 +0530

    trying to fix reviewer error

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,25 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import {Project} from '@/models/Project';
+import Task from "@/models/Task";
+import { Template } from '@/models/Template';
+// Import all other models here
+
 const { MONGODB_URI } = process.env;
+
 export const connectToDatabase = async () => {
   try {
+    // Initialize all models to ensure they are registered
+    if (!Project || !Task || !Template) {
+      throw new Error("Models not initialized properly.");
+    }
+
     const { connection } = await mongoose.connect(MONGODB_URI as string);
     if (connection.readyState === 1) {
+      console.log('Database connection established.');
       return Promise.resolve(true);
     }
   } catch (error) {
-    console.error(error);
+    console.error('Error connecting to the database:', error);
     return Promise.reject(error);
   }
 };
