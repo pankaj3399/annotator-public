3785b11c0acaeb9b6c1643623ff7e934363c4cb0
Absterrg0 <parvj5212@gmail.com>
2024-12-20 19:02:12 +0530

    Implemented more annotator API's

---
+++ app/api/Task/review/getAllAnnotators/route.ts
@@ -0,0 +1,32 @@
+import { getAllAnnotators } from "@/app/actions/annotator";
+import { authOptions } from "@/auth";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+
+
+
+
+export async function GET(){
+    // const session = await getServerSession(authOptions);
+    // if(!session||!session.user){
+    //     return NextResponse.json({
+    //         msg:"Unauthorized"
+    //     },{
+    //         status:403
+    //     })
+    // }
+
+    try{
+        const response = await getAllAnnotators();
+        return NextResponse.json(JSON.parse(response),{
+            status:200
+        })
+    }
+    catch(e){
+        console.error(e);
+        return NextResponse.json({
+            msg:"Error while fetching all the annotators"
+        })
+    }
+}
