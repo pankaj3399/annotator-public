7d88f8d1b11d87cf41fc6a1eb4c6a5affc71b075
Absterrg0 <parvj5212@gmail.com>
2024-12-28 13:42:46 +0530

    Fixed_sanity_placeholder

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "d1xmepf1",
+  projectId: process.env.SANITY_ID,
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
