dd49ae5936c38c11bd54102f01bf7282f41ebceb
Absterrg0 <parvj5212@gmail.com>
2025-02-02 23:25:51 +0530

    Updating it again

--- lib/db.ts
+++ lib/db.ts
@@ -1,23 +1,66 @@
-import mongoose from 'mongoose';
+import { MongoClient } from 'mongodb';
 
-const { MONGODB_URI } = process.env;
-
-let isConnected: boolean = false; // Track connection status
-
-export const connectToDatabase = async () => {
-  if (isConnected) {
-    console.log('Already connected to the database.');
-    return Promise.resolve(true);
-  }
-
-  try {
-    // Connect to the database
-    await mongoose.connect(MONGODB_URI as string);
-    isConnected = true; // Update connection status
-    console.log('Database connection established.');
-    return Promise.resolve(true);
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    return Promise.reject(error);
-  }
+const uri = process.env.MONGODB_URI as string; // your mongodb connection string
+const options = {
+  useUnifiedTopology: true,
+  minPoolSize: 5,  // Minimum number of connections in the pool
+  maxPoolSize: 70, // Maximum number of connections in the pool
 };
+
+declare global {
+  var _mongoClientPromise: Promise<MongoClient>;
+}
+
+class Singleton {
+  private static _instance: Singleton;
+  private _client: MongoClient;
+  private _clientPromise: Promise<MongoClient>;
+
+  private constructor() {
+    this._client = new MongoClient(uri, options);
+    this._clientPromise = this._client.connect();
+    if (process.env.NODE_ENV === 'development') {
+      // In development mode, use a global variable so that the value
+      // is preserved across module reloads caused by HMR (Hot Module Replacement).
+      global._mongoClientPromise = this._clientPromise;
+    }
+  }
+
+  public static get instance() {
+    if (!this._instance) {
+      this._instance = new Singleton();
+    }
+    return this._instance;
+  }
+
+  public get clientPromise() {
+    return this._clientPromise;
+  }
+
+  public get client() {
+    return this._client;
+  }
+
+  public get db() {
+    return this._client.db(process.env.MONGODB_DB as string);
+  }
+}
+
+const singleton = Singleton.instance;
+
+// The connectToDatabase function
+export async function connectToDatabase() {
+  await singleton.clientPromise;
+  return { client: singleton.client, db: singleton.db };
+}
+
+export async function disconnectFromDatabase() {
+  /*if (cachedClient && !isConnecting) {
+    await cachedClient.close();
+    console.log('MongoDB connection closed');
+    cachedClient = null;
+    cachedDb = null;
+  } else {
+    console.log('No active MongoDB connection to close or connection is in the process of connecting');
+  }*/
+}
