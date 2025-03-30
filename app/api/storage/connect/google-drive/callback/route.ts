b8d8f682106e45c856e749af7f9c66d9a69636c0
Ana Fariya <anafariya@gmail.com>
2025-03-30 20:48:24 +0530

    debugging drive connect log out

--- app/api/storage/connect/google-drive/callback/route.ts
+++ app/api/storage/connect/google-drive/callback/route.ts
@@ -5,7 +5,7 @@ import { google } from "googleapis";
 import mongoose from "mongoose";
 import { connectToDatabase } from "@/lib/db";
 const nextAuthUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
-const redirectUri = `${nextAuthUrl}api/auth/callback/google`;
+const redirectUri = `${nextAuthUrl}/api/storage/connect/google-drive/callback`;
 // Your Google OAuth credentials from environment variables
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
