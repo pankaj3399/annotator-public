fbd602cf6187f9ecde9a0e50d95e04b8ac0e70b5
Ana Fariya <anafariya@gmail.com>
2025-04-28 19:30:17 +0530

    Side bar UI  Changes

--- components/TemplateIcon.tsx
+++ components/TemplateIcon.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import Template from "@/public/icons/template.jpg";
 
 export const TemplateIcon = () => {
-  return <Image src={Template} alt="Book" width={18} height={18} />;
+  return <Image src={Template} alt="Book" width={17} height={17} className="mr-3" />;
 };
