88acaa2455467d9ac2083c8aeadbed8d8aac4df4
Ana Fariya <anafariya@gmail.com>
2025-05-11 19:18:52 +0530

    Refactor next.config.mjs: remove commented-out headers configuration and add experimental serverActions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -17,7 +15,7 @@ const nextConfig = {
   images: {
     domains: ['via.placeholder.com', 'cdn.sanity.io', 'annotator-public.s3.ap-south-1.amazonaws.com'],
   },
-
+  // Add these new configurations
   experimental: {
     serverActions: true,
   },
@@ -28,26 +26,8 @@ const nextConfig = {
         destination: '/api/upload/:path*',
       }
     ];
-  },
-
-  async headers() {
-    const defaultAllowedOrigin = process.env.NODE_ENV === 'production'
-      ? 'https://www.blolabel.ai'
-      : 'http://localhost:3000';
-
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: defaultAllowedOrigin,
-          },
-        ],
-      },
-    ];
-  },
+  }
 };
 
-
+// Keep export default since we're using ES modules
 export default nextConfig;
